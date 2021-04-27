9704871d52242807ea64a8687eb1e1008d69a4da
gideongurning31 <gideon.gurning@gmail.com>
2021-04-27 21:37:50 +0700

    Lisa's Workbook (problem solving)

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,36 @@
 import java.io.*;
 
 class Result {
-  public static long minimumDistances(int[] a) {
-    long minDistance = 0;
-    for (int i = 0; i < a.length; i++) {
-      for (int j = i + 1; j < a.length; j++) {
-        if (a[i] == a[j]) {
-          if (minDistance == 0) minDistance = j - i;
-          minDistance = Math.min(minDistance, j - i);
-        }
+
+  public static int specialProblems = 0;
+  private static int currentPage = 0;
+  private static int problemNo = 1;
+
+  private static void isSpecialProblem(int problemsInChapter, int limit) {
+    while (problemsInChapter != 0 && limit != 0) {
+      if (problemNo == currentPage) specialProblems++;
+      problemNo++;
+      problemsInChapter--;
+      limit--;
+    }
+  }
+
+  public static int workbook(int limit, int[] chapters) {
+    int i = 0;
+    while (i < chapters.length) {
+      currentPage++;
+      isSpecialProblem(chapters[i], limit);
+
+      if (limit >= chapters[i]) {
+        problemNo = 1;
+        i++;
+      } else {
+        problemNo += problemNo == 1 ? limit : 0;
+        chapters[i] -= limit;
       }
     }
 
-    return minDistance == 0 ? -1 : minDistance;
+    return specialProblems;
   }
 }
 
@@ -20,14 +38,19 @@ public class Solution {
   public static void main(String[] args) throws IOException {
     BufferedReader bufferedReader = new BufferedReader(new FileReader("input.txt"));
     BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output.txt"));
-    int n = Integer.parseInt(bufferedReader.readLine().trim());
-    int[] x = new int[n];
-    String[] a = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+    String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+    int n = Integer.parseInt(firstMultipleInput[0]);
+    int k = Integer.parseInt(firstMultipleInput[1]);
+
+    String[] s = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+    int[] arr = new int[n];
     for (int i = 0; i < n; i++) {
-      x[i] = Integer.parseInt(a[i]);
+      arr[i] = Integer.parseInt(s[i]);
     }
 
-    bufferedWriter.write(String.valueOf(Result.minimumDistances(x)));
+    int result = Result.workbook(k, arr);
+    bufferedWriter.write(String.valueOf(result));
     bufferedWriter.newLine();
     bufferedReader.close();
     bufferedWriter.close();
