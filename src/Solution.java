3b653b81cf7ca6ce6618a957caf8a6c3190cfc3e
gideongurning31 <gideon.gurning@gmail.com>
2021-04-27 11:14:10 +0700

    Minimum Distances (problem solving)

--- src/Solution.java
+++ src/Solution.java
@@ -1,21 +1,18 @@
 import java.io.*;
 
 class Result {
-  public static int beautifulTriplets(int d, int[] arr) {
-    int result = 0;
-    int L = arr.length;
-
-    for (int i = 0; i < L; i++) {
-      for (int j = i + 1; j < L; j++) {
-        if (arr[j] - arr[i] == d) {
-          for (int k = j + 1; k < L; k++) {
-            if (arr[k] - arr[j] == d) result++;
-          }
+  public static long minimumDistances(int[] a) {
+    long minDistance = 0;
+    for (int i = 0; i < a.length; i++) {
+      for (int j = i + 1; j < a.length; j++) {
+        if (a[i] == a[j]) {
+          if (minDistance == 0) minDistance = j - i;
+          minDistance = Math.min(minDistance, j - i);
         }
       }
     }
 
-    return result;
+    return minDistance == 0 ? -1 : minDistance;
   }
 }
 
@@ -23,16 +20,14 @@ public class Solution {
   public static void main(String[] args) throws IOException {
     BufferedReader bufferedReader = new BufferedReader(new FileReader("input.txt"));
     BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output.txt"));
-    String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-    int d = Integer.parseInt(firstMultipleInput[1]);
-
-    String[] arrStr = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-    int[] arr = new int[arrStr.length];
-    for (int i = 0; i < arrStr.length; i++) {
-      arr[i] = Integer.parseInt(arrStr[i]);
+    int n = Integer.parseInt(bufferedReader.readLine().trim());
+    int[] x = new int[n];
+    String[] a = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+    for (int i = 0; i < n; i++) {
+      x[i] = Integer.parseInt(a[i]);
     }
 
-    bufferedWriter.write(String.valueOf(Result.beautifulTriplets(d, arr)));
+    bufferedWriter.write(String.valueOf(Result.minimumDistances(x)));
     bufferedWriter.newLine();
     bufferedReader.close();
     bufferedWriter.close();
