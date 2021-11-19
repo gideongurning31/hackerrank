0840ce216af685ac88ec97e79accab49477959cb
gideongurning31 <gideon.gurning@gmail.com>
2021-11-20 00:11:15 +0700

    Codility Parking Bill solution

--- src/Solution.java
+++ src/Solution.java
@@ -2,41 +2,40 @@ import java.io.*;
 
 public class Solution {
 
-  private static int longestGap = 0;
-
   public static void main(String[] args) throws IOException {
     BufferedReader bufferedReader = new BufferedReader(new FileReader("input.txt"));
     BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output.txt"));
 
     String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 
-    for (String n : firstMultipleInput) {
-      int value = Integer.parseInt(n);
-      System.out.println("Result: ".concat(String.valueOf(longestBinaryGap(value))));
-    }
+    String enter = firstMultipleInput[0];
+    String leave = firstMultipleInput[1];
+    System.out.println("Result: ".concat(String.valueOf(parkingFee(enter, leave))));
   }
 
-  private static int longestBinaryGap(int N) {
-    String binary = Integer.toBinaryString(N);
-    if (binary.length() <= 1) {
-      return 0;
+  private static final int ENTRANCE_FEE = 2;
+  private static final int FIRST_HOUR_FEE = 3;
+  private static final int OVER_ONE_HOUR_FEE = 4;
+
+  private static int parkingFee(String E, String L) {
+    String[] enter = E.split(":");
+    String[] leave = L.split(":");
+
+    int enterHour = Integer.parseInt(enter[0]);
+    int leaveHour = Integer.parseInt(leave[0]);
+    int enterMinute = Integer.parseInt(enter[1]);
+    int leaveMinute = Integer.parseInt(leave[1]);
+
+    if (enterHour == leaveHour || (leaveHour - enterHour == 1 && enterMinute >= leaveMinute)) {
+      return ENTRANCE_FEE + FIRST_HOUR_FEE;
     }
 
-    String[] binarySpliced = binary.replace("1", "#1").split("#");
-
-    for (int i = 0; i < binarySpliced.length - 1; i++) {
-      if (!binarySpliced[i + 1].equals("0")) {
-        System.out.println(binarySpliced[i]);
-        setLongestGap(binarySpliced[i].length() - 1);
-      }
-    }
-
-    return longestGap;
+    return ENTRANCE_FEE + FIRST_HOUR_FEE + calculateOver2Hour(enterHour, enterMinute, leaveHour, leaveMinute);
   }
 
-  private static void setLongestGap(int gap) {
-    if (longestGap < gap) {
-      longestGap = gap;
-    }
+  private static int calculateOver2Hour(int eH, int eM, int lH, int lM) {
+    int overByMinute = eM >= lM ? 0 : 1;
+    int overByHour = lH - eH - 1;
+    return (overByHour + overByMinute)*OVER_ONE_HOUR_FEE;
   }
 }
