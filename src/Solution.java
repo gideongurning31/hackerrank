1133b3f20f21a6504bf668ef29a0a341bd0b9550
gideongurning31 <gideon.gurning@gmail.com>
2021-11-20 16:52:05 +0700

    Codility permMissingElement solution

--- src/Solution.java
+++ src/Solution.java
@@ -6,24 +6,28 @@ public class Solution {
     BufferedReader bufferedReader = new BufferedReader(new FileReader("input.txt"));
     String[] textInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 
-    int x = Integer.parseInt(textInput[0]);
-    int y = Integer.parseInt(textInput[1]);
-    int d = Integer.parseInt(textInput[2]);
-
-    System.out.println("Result: ".concat(String.valueOf(frogJump(x, y, d))));
-  }
-
-  private static int frogJump(int x, int y, int d) {
-    int distance = y - x;
-    int distanceRemainder = distance % d;
-    int roundDistance = distance - distanceRemainder;
-    int totalJumped = roundDistance / d;
-
-    if (distanceRemainder > 0) {
-      return totalJumped + 1;
+    int[] input = new int[textInput.length];
+    for (int i = 0; i < input.length; i++) {
+      input[i] = Integer.parseInt(textInput[i]);
     }
 
-    return totalJumped;
+    System.out.println("Result: ".concat(String.valueOf(permMissingElement(input))));
+  }
+
+  private static int permMissingElement(int[] A) {
+    int[] completeArr = new int[A.length + 1];
+
+    for (int x : A) {
+      completeArr[x - 1] = x;
+    }
+
+    for (int i = 0; i < completeArr.length; i++) {
+      if (completeArr[i] == 0) {
+        return i + 1;
+      }
+    }
+
+    return 0;
   }
 
 }
