0cf2402bfbfbd9bb403f7c9961966a54078558c9
gideongurning31 <gideon.gurning@gmail.com>
2021-11-20 12:28:31 +0700

    Codility array rotation solution

--- src/Solution.java
+++ src/Solution.java
@@ -1,41 +1,43 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class Solution {
 
   public static void main(String[] args) throws IOException {
     BufferedReader bufferedReader = new BufferedReader(new FileReader("input.txt"));
-    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output.txt"));
+    String[] textInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 
-    String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
-    String enter = firstMultipleInput[0];
-    String leave = firstMultipleInput[1];
-    System.out.println("Result: ".concat(String.valueOf(parkingFee(enter, leave))));
-  }
-
-  private static final int ENTRANCE_FEE = 2;
-  private static final int FIRST_HOUR_FEE = 3;
-  private static final int OVER_ONE_HOUR_FEE = 4;
-
-  private static int parkingFee(String E, String L) {
-    String[] enter = E.split(":");
-    String[] leave = L.split(":");
-
-    int enterHour = Integer.parseInt(enter[0]);
-    int leaveHour = Integer.parseInt(leave[0]);
-    int enterMinute = Integer.parseInt(enter[1]);
-    int leaveMinute = Integer.parseInt(leave[1]);
-
-    if (enterHour == leaveHour || (leaveHour - enterHour == 1 && enterMinute >= leaveMinute)) {
-      return ENTRANCE_FEE + FIRST_HOUR_FEE;
+    int k = Integer.parseInt(textInput[1]);
+    String[] arrayInput = textInput[0].split(",");
+    int[] input = new int[arrayInput.length];
+    for (int i = 0; i < arrayInput.length; i++) {
+      input[i] = Integer.parseInt(arrayInput[i]);
     }
 
-    return ENTRANCE_FEE + FIRST_HOUR_FEE + calculateOver2Hour(enterHour, enterMinute, leaveHour, leaveMinute);
+    System.out.println("Result: ".concat(Arrays.toString(arrayRotation(input, k))));
   }
 
-  private static int calculateOver2Hour(int eH, int eM, int lH, int lM) {
-    int overByMinute = eM >= lM ? 0 : 1;
-    int overByHour = lH - eH - 1;
-    return (overByHour + overByMinute)*OVER_ONE_HOUR_FEE;
+  private static int[] result;
+
+  private static int[] arrayRotation(int[] arr, int K) {
+    result = arr;
+
+    if (arr.length == 0) {
+      return arr;
+    }
+
+    while (K != 0) {
+      rotateOnce();
+      K--;
+    }
+
+    return result;
+  }
+
+  private static void rotateOnce() {
+    int n = result.length;
+    final int nthValue = result[n - 1];
+    System.arraycopy(result, 0, result, 1, n - 1);
+    result[0] = nthValue;
   }
 }
